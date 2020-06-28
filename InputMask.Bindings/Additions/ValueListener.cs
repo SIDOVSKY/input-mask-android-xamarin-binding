b3fbc51e46aff3883e719a494edab150c8674c16
Vadim Sedov <sidovsky.develop@gmail.com>
2020-06-28 17:58:26 +0700

    Initial commit

---
+++ InputMask.Bindings/Additions/ValueListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InputMask
+{
+    public class ValueListener : Java.Lang.Object, MaskedTextChangedListener.IValueListener
+    {
+        private readonly Action<bool, string, string> _onTextChanged;
+
+        public ValueListener(Action<bool, string, string> onTextChanged)
+        {
+            _onTextChanged = onTextChanged;
+        }
+
+        public void OnTextChanged(bool maskFilled, string extractedValue, string formattedValue)
+        {
+            _onTextChanged?.Invoke(maskFilled, extractedValue, formattedValue);
+        }
+    }
+}
---
+++ InputMask.Sample/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Util;
+using Android.Widget;
+using InputMask.Helper;
+using System.Collections.Generic;
+
+namespace InputMask.Sample
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            SetupPrefixSample();
+            SetupSuffixSample();
+        }
+
+        private void SetupPrefixSample()
+        {
+            var editText = FindViewById<EditText>(Resource.Id.prefix_edit_text);
+            var checkBox = FindViewById<CheckBox>(Resource.Id.prefix_check_box);
+            var affineFormats = new List<string>
+            {
+                "8 ([000]) [000]-[00]-[00]"
+            };
+
+            var listener = MaskedTextChangedListener.Companion.InstallOn(editText,
+                "+7 ([000]) [000]-[00]-[00]",
+                affineFormats, AffinityCalculationStrategy.Prefix,
+                new ValueListener((maskFilled, extractedValue, formattedValue) =>
+                {
+                    LogValueListener(maskFilled, extractedValue, formattedValue);
+                    checkBox.Checked = maskFilled;
+                }));
+
+            editText.Hint = listener.Placeholder();
+        }
+
+        private void SetupSuffixSample()
+        {
+            var editText = FindViewById<EditText>(Resource.Id.suffix_edit_text);
+            var checkBox = FindViewById<CheckBox>(Resource.Id.suffix_check_box);
+            var affineFormats = new List<string>
+            {
+                "+7 ([000]) [000]-[00]-[00]#[000]"
+            };
+
+            var listener = MaskedTextChangedListener.Companion.InstallOn(editText,
+                "+7 ([000]) [000]-[00]-[00]",
+                affineFormats, AffinityCalculationStrategy.WholeString,
+                new ValueListener((maskFilled, extractedValue, formattedValue) =>
+                {
+                    LogValueListener(maskFilled, extractedValue, formattedValue);
+                    checkBox.Checked = maskFilled;
+                }));
+
+            editText.Hint = listener.Placeholder();
+        }
+
+        private void LogValueListener(bool maskFilled, string extractedValue, string formattedValue)
+        {
+            var tag = nameof(MainActivity);
+            Log.Debug(tag, maskFilled.ToString());
+            Log.Debug(tag, extractedValue);
+            Log.Debug(tag, formattedValue);
+        }
+    }
+}
